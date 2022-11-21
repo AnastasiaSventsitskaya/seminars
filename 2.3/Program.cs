d8135d8a20e5e29b61eb48c0a1d7e6c2fbefdc0c
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-21 20:45:25 +0400

    является ли одно квадратом другого

---
+++ 2.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// принимает 2 чила и проверяет является ли одно квадратом другого.
+
+int num1 = new Random().Next(10,100);
+int num2 = new Random().Next(10,100);
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+
+if ((num1==num2*num2)||(num2==num1*num1))
+  Console.WriteLine("yes");
+
+else 
+  Console.WriteLine("no");
+
