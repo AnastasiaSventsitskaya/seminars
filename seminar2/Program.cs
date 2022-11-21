5f39468c0822e2fdf493e6e9f8bf934ac39f4d98
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-21 19:51:20 +0400

    удаление второго числа из трехзначного числа

---
+++ seminar2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу , которая выводит рандомное трехзначное число и удаляет вторую цифру этого числа
+
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+
+int result = (num/100)*10 + num%10;
+
+Console.WriteLine(result);
