07ccbbd060143ccc80dcbb0cd47c10027a5facfe
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-21 20:17:48 +0400

    случаяйное число кратное 23 и 7

---
+++ seminar2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿// вывести число и проверить кратно ли оно 23 и 7 одновременно
+
+int num = new Random().Next(10,100);
+Console.WriteLine(num);
+
+if ((num%7==0) && (num%23==0))
+{
+    Console.WriteLine("number"+ num + "yes 23 and yes 7");
+}
+
+else
+{
+    Console.WriteLine("no");
+}
