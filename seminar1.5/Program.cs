afb5ecd6d85ec2764929a6541651225efccd23ff
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-21 18:34:17 +0400

    5

---
+++ seminar1.5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите задачу, которая на вход прирнимает трехзначное число и на выходе показывает последнюю цифру этого числа.
+//456->6
+
+Console.WriteLine("Write number: ");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int result=num%10;
+Console.WriteLine (result);
---
+++ seminar1/Program.cs
@@ -0,0 +1,9 @@
+﻿// 4->16 -3
+Console.WriteLine("write number");
+int num= Convert.ToInt32(Console.ReadLine());
+int sqt = num*num;
+Console.WriteLine("number"+sqt); 
+
+
+
+
