053c67fafe05c18696895689f8a4a30695a28ba8
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-21 17:39:03 +0400

    1

---
+++ seminar1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 
+
+Console.WriteLine("write number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("write number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num == num2*num2)
+{
+   Console.WriteLine("Number" + num + "Является квадратом числа" + num2);
+}
+else
+{
+   Console.WriteLine("Number" + num + "Не является квадратом числа" + num2);
+}
