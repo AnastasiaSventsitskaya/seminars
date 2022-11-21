8bee3cac7c8d42721a5cfbb14d90e02309c051ef
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-21 18:23:27 +0400

    4

---
+++ seminar1.4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, оторая на входе принимает одно число, а на выхоже выдает все целые числа. 
+// 4--> -4 -3 -2 -1 0 1 2 3 4
+
+Console.WriteLine("Write number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int negNum = -num;
+while(negNum<=num)
+{
+    Console.WriteLine(negNum);
+    negNum++;
+}
