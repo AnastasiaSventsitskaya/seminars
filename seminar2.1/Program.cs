71e4c3e26544fe0d58ab9441ae6e2c4f5eaf7520
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-21 20:08:00 +0400

    остаток от деления

---
+++ seminar2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа принимает два числа и выводить является ли второе кратным первому. если не кратно, то выводим остаток от деления
+
+
+int num1 = new Random().Next(10,100);
+int num2 = new Random().Next(10,100);
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+
+if (num1%num2==0)
+{
+    Console.WriteLine("Число"+num1+ "кратное числу" + num2);
+}
+else 
+{
+    Console.WriteLine("Остаток от деления"+num1%num2);
+}
