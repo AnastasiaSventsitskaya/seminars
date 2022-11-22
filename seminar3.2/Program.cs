c58859e73f569026d626c4288a4bd888d5a14305
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-22 20:17:37 +0400

    3.4

---
+++ seminar3.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// напишите программу, которая принимает на вход число  и выдает таблицу квадратов чисел от 1 до этого числа
+
+
+Console.WriteLine("Write number");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+for (var counter = 1; counter <= n; counter++) 
+{
+  Console.Write(counter * counter + " ");
+}
