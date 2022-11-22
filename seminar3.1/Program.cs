0bf930e4f63b310b0a6d56f1746b26e4c5af399a
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-22 19:57:01 +0400

    3.3

---
+++ seminar3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// напишите программу, которая при вводе точек , находит расстояние между ними в 2д пространстве
+
+Console.WriteLine("write X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("write Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("write X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("write Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2));
+
+Console.WriteLine(result);
