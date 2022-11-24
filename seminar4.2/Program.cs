c9db2dbc42e90150582083e0bb27c26b6a364f9f
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-24 20:31:07 +0400

    4.2

---
+++ seminar4.2/Program.cs
@@ -0,0 +1,22 @@
+﻿//принимает число и выдает произведение от 1 до этого числа
+
+
+
+Console.WriteLine("Write number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(GetSumNum(num));
+
+int GetSumNum(int res)
+{
+ int result = 1;
+ int count = 1;
+
+ while(res>=count)
+ {
+    result *= count;
+    count++;
+ }
+ return result;
+}
+
