0595cc5f761f05e751d96543ed1a9c89377afda0
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-24 20:42:32 +0400

    4.3

---
+++ seminar4.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа , которая выводит массив элементов, заполненный нуями и единицами в случайном порядке
+
+int [] GetArray(int size)
+{
+    int[] array1 = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array1[i] = new Random().Next(0,2);
+    }
+    return array1;
+}
+int[] array = GetArray(8);
+
+Console.Write(String.Join(",",array));
