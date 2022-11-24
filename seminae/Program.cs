05232dbde51d55ff5d7daf4e770af0272679f19b
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-24 20:11:55 +0400

    4.1

---
+++ seminae/Program.cs
@@ -0,0 +1,20 @@
+﻿// принимает на вход число и выдает кол-во цифр в числе
+
+Console.WriteLine("Write number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Колличество цифр" + GetCountNum(num));
+
+int GetCountNum(int res)
+{
+    int count = 0;
+    while(res>0)
+    {
+        count++;
+        res/=10;
+    }
+    return count;
+}
+
+
+
